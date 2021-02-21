5bcf456c773018d22bd180ad623c690056fd0b7a
BaronBrabant <71703295+BaronBrabant@users.noreply.github.com>
2021-02-21 10:57:14 +0100

    Create specification_draft.py

---
+++ specification_draft.py
@@ -0,0 +1,19 @@
+def colour_code_map(dico2, list_position_earth):
+    '''
+    This function 
+
+    Parameters
+    __________
+
+    dico2 : dictionnary containting list filled with infomation on ants (list1)
+    list1[0] : contains force of ants to determin their strength to carry earth (int)
+    list1[1] : contains boolean value determining if the ant is already carrying something (bool)
+    list_position_earth : list containing all pieces of earth (list)
+
+    Return
+    ______ 
+
+    allowed_or_not : boolean value determining if the piece of earth can be picked up (bool)
+    '''
+
+    return allowed_or_not
